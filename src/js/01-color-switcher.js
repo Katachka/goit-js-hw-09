80f00f18ff7b80787a2d6ddbd73dbeb88f4b9be4
Kateryna Ponomarenko <katachka.p@gmail.com>
2023-02-25 00:07:24 +0200

    task-01

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,40 @@
+const refs = {
+  startBtn: document.querySelector('button[data-start]'),
+  stopBtn: document.querySelector('button[data-stop]'),
+};
 
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+}
+// function setBodyBgColor(color) {
+//   document.body.style.backgroundColor = color;
+// }
+class ColorSwitcher {
+  constructor() {
+    this.intervalID = null;
+    this.isActive = false;
+  }
+
+  start() {
+    if (this.isActive) {
+      return;
+    }
+    refs.startBtn.disabled = true;
+    refs.stopBtn.disabled = false;
+
+    this.isActive = true;
+    this.intervalID = setInterval(() => {
+      document.body.style.backgroundColor = getRandomHexColor();
+    }, 1000);
+  }
+  stop() {
+    this.isActive = false;
+    refs.startBtn.disabled = false;
+    refs.stopBtn.disabled = true;
+    clearInterval(this.intervalID);
+  }
+}
+const colorSwitcher = new ColorSwitcher();
+
+refs.startBtn.addEventListener('click', colorSwitcher.start());
+refs.stopBtn.addEventListener('click', colorSwitcher.stop());
