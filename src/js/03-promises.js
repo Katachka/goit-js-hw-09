efefa97257c72de0d254be075d4d36d53daaffac
Kateryna Ponomarenko <katachka.p@gmail.com>
2023-02-27 00:15:51 +0200

    task-03

--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -1,8 +1,38 @@
-function createPromise(position, delay) {
-  const shouldResolve = Math.random() > 0.3;
-  if (shouldResolve) {
-    // Fulfill
-  } else {
-    // Reject
+import { Notify } from 'notiflix/build/notiflix-notify-aio';
+
+const formRef = document.querySelector('.form');
+formRef.addEventListener('submit', onSubmitForm);
+
+function onSubmitForm(e) {
+  e.preventDefault();
+
+  let delay = Number(formRef.delay.value);
+
+  for (let position = 1; position <= formRef.amount.value; position++) {
+    createPromise(position, delay)
+      .then(({ position, delay }) => {
+        Notify.success(`✅ Fulfilled promise ${position} in ${delay}ms`);
+        console.log(`✅ Fulfilled promise ${position} in ${delay}ms`);
+      })
+      .catch(({ position, delay }) => {
+        Notify.failure(`❌ Rejected promise ${position} in ${delay}ms`);
+        console.log(`❌ Rejected promise ${position} in ${delay}ms`);
+      });
+    delay += Number(formRef.step.value);
   }
+}
+
+function createPromise(position, delay) {
+  const objectPromise = { position, delay };
+  const shouldResolve = Math.random() > 0.3;
+
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
+      if (shouldResolve) {
+        resolve(objectPromise);
+      } else {
+        reject(objectPromise);
+      }
+    }, delay);
+  });
 }
